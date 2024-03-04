846f32ff6d07606afa863ce9d16d0f5ada3efff8
NKJ14 <nishanthere1402@gmail.com>
2024-03-04 22:51:52 +0530

    Frontend init. Installed tailwind + postcss, react-router-dom.

---
+++ medium-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='p-10 text-slate-400 hover:text-red-400'>
+      hi there
+      </div>
+    </>
+  )
+}
+
+export default App
